bf1589adff4dff40de5ead8bb3d3bc367491b54e
ivan-josef <ivanjosef141@gmail.com>
2024-08-06 12:17:12 -0300

    test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
 #include "InIndKit.h"
 
+
+AsyncDelay_c delayPOT(100); // time mili second
+
 void setup()
 {
-    InIndKit.setup("Wokwi-GUEST", "", "inindkit10");
+    InIndKit.setup("Wokwi-GUEST", "");
+    Serial.begin(9600);
 }
 
 void loop()
 {
     InIndKit.loop();
+    if (delayPOT.isExpired())
+    {
+        delayPOT.repeat();
+        const uint16_t pot_left = analogRead(def_pin_POT_LEFT);
+        WSerial.println(pot_left);
+    }
 }
